55e299b41f45f9b374e9a1d65c515852dc06a640
OSAMU WATANABE <owatanab@es.takushoku-u.ac.jp>
2021-07-02 16:01:02 +0900

    fix to avoid build error on MSYS2

--- source/core/transform/dwt.hpp
+++ source/core/transform/dwt.hpp
@@ -34,10 +34,8 @@
   #include <omp.h>
 #endif
 
-#if defined(_MSC_VER)
+#if defined(_MSC_VER) || defined(__MINGW64__)
   #include <intrin.h>
-#elif defined(__MINGW32__) || defined(__MINGW64__)
-  #include <cpuid.h>
 #elif defined(__x86_64__)
   #include <x86intrin.h>
 #endif
