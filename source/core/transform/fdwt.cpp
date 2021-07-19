8619346a49d0e21ed07be4b825eb4ce6c11e6132
OSAMU WATANABE <owatanab@es.takushoku-u.ac.jp>
2021-07-19 17:24:19 +0900

    modify Horizontal fdwt buffering structure

--- source/core/transform/fdwt.cpp
+++ source/core/transform/fdwt.cpp
@@ -87,16 +87,14 @@ static fdwt_1d_filtr_func_fixed fdwt_1d_filtr_fixed[2] = {fdwt_1d_filtr_irrev97_
                                                           fdwt_1d_filtr_rev53_fixed};
 
 // 1-dimensional FDWT
-static inline void fdwt_1d_sr_fixed(int16_t *in, int16_t *out, const int32_t left, const int32_t right,
+static inline void fdwt_1d_sr_fixed(int16_t *buf, int16_t *in, int16_t *out, const int32_t left, const int32_t right,
                                     const uint32_t i0, const uint32_t i1, const uint8_t transformation) {
-  const uint32_t len = round_up(i1 - i0 + left + right, SIMD_LEN_I16);
-  auto *Xext         = static_cast<int16_t *>(aligned_mem_alloc(sizeof(int16_t) * len, 32));
-  // auto *Yext         = static_cast<int16_t *>(aligned_mem_alloc(sizeof(int16_t) * len, 32));
-  dwt_1d_extr_fixed(Xext, in, left, right, i0, i1);
-  fdwt_1d_filtr_fixed[transformation](Xext, left, right, i0, i1);
-  memcpy(out, Xext + left, sizeof(int16_t) * (i1 - i0));
-  aligned_mem_free(Xext);
-  // aligned_mem_free(Yext);
+//  const uint32_t len = round_up(i1 - i0 + left + right, SIMD_LEN_I16);
+//  auto *Xext         = static_cast<int16_t *>(aligned_mem_alloc(sizeof(int16_t) * len, 32));
+  dwt_1d_extr_fixed(buf, in, left, right, i0, i1);
+  fdwt_1d_filtr_fixed[transformation](buf, left, right, i0, i1);
+  memcpy(out, buf + left, sizeof(int16_t) * (i1 - i0));
+//  aligned_mem_free(Xext);
 }
 
 // FDWT for horizontal direction
@@ -121,10 +119,13 @@ static void fdwt_hor_sr_fixed(int16_t *out, int16_t *in, const uint32_t u0, cons
     }
   } else {
     // need to perform symmetric extension
+    const uint32_t len = round_up(u1 - u0 + left + right, SIMD_LEN_I16);
+    auto *Xext         = static_cast<int16_t *>(aligned_mem_alloc(sizeof(int16_t) * len, 32));
     //#pragma omp parallel for
     for (uint32_t row = 0; row < v1 - v0; ++row) {
-      fdwt_1d_sr_fixed(&in[row * stride], &out[row * stride], left, right, u0, u1, transformation);
+      fdwt_1d_sr_fixed(Xext, &in[row * stride], &out[row * stride], left, right, u0, u1, transformation);
     }
+    aligned_mem_free(Xext);
   }
 }
 
