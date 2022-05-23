d64684654f4a13a97e1358aa4e50960ac9ce9c4a
OSAMU WATANABE <owatanab@es.takushoku-u.ac.jp>
2022-05-23 23:14:50 +0900

    change include path for ctest

--- tests/decoder_conformance.cmake
+++ tests/decoder_conformance.cmake
@@ -1,15 +1,15 @@
 # Conformance testing by Ctest
 enable_testing()
-set(CONFORMANCE_DATA_DIR "${CMAKE_SOURCE_DIR}/conformance_data")
+set(CONFORMANCE_DATA_DIR "${CMAKE_CURRENT_SOURCE_DIR}/conformance_data")
 
 ## Conformance tests for HT
 # PROFILE 0
-include(${CMAKE_SOURCE_DIR}/tests/ht_profile0.cmake)
+include(${CMAKE_CURRENT_SOURCE_DIR}/tests/ht_profile0.cmake)
 # PROFILE 1
-include(${CMAKE_SOURCE_DIR}/tests/ht_profile1.cmake)
+include(${CMAKE_CURRENT_SOURCE_DIR}/tests/ht_profile1.cmake)
 ## Part 1 decoding tests
 # PROFILE 0
-include(${CMAKE_SOURCE_DIR}/tests/part1_profile0.cmake)
+include(${CMAKE_CURRENT_SOURCE_DIR}/tests/part1_profile0.cmake)
 # PROFILE 1
-include(${CMAKE_SOURCE_DIR}/tests/part1_profile1.cmake)
+include(${CMAKE_CURRENT_SOURCE_DIR}/tests/part1_profile1.cmake)
 
