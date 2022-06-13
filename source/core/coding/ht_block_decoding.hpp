fab307116fd2d2148dcbe53b046f7b731e49e3d0
OSAMU WATANABE <owatanab@es.takushoku-u.ac.jp>
2022-06-13 18:31:52 +0900

    test

--- source/core/coding/ht_block_decoding.hpp
+++ source/core/coding/ht_block_decoding.hpp
@@ -37,6 +37,151 @@ const int32_t bitmask32[32] = {
     0x0000FFFF, 0x0001FFFF, 0x0003FFFF, 0x0007FFFF, 0x000FFFFF, 0x001FFFFF, 0x003FFFFF, 0x007FFFFF,
     0x00FFFFFF, 0x01FFFFFF, 0x03FFFFFF, 0x07FFFFFF, 0x0FFFFFFF, 0x1FFFFFFF, 0x3FFFFFFF, 0x7FFFFFFF};
 /********************************************************************************
+ * MEL_dec:
+ *******************************************************************************/
+class MEL_dec {
+ private:
+  int bits;
+  uint64_t tmp;
+  const uint8_t *buf;
+  int32_t length;
+  bool unstuff;
+  int MEL_k;
+
+  int32_t num_runs;
+  uint64_t runs;
+
+ public:
+  MEL_dec(const uint8_t *Dcup, uint32_t Lcup, int32_t Scup)
+      : bits(0),
+        tmp(0),
+        buf(Dcup + Lcup - Scup),
+        length(Scup - 1),  // length is the length of MEL+VLC-1
+        unstuff(false),
+        MEL_k(0),
+        num_runs(0),
+        runs(0) {
+    int num = 4 - static_cast<int>(reinterpret_cast<intptr_t>(buf) & 0x3);
+    for (int32_t i = 0; i < num; ++i) {
+      uint64_t d = (length > 0) ? *buf : 0xFF;  // if buffer is exhausted, set data to 0xFF
+      if (length == 1) {
+        d |= 0xF;  // if this is MEL+VLC+1, set LSBs to 0xF (see the spec)
+      }
+      buf += length-- > 0;  // increment if the end is not reached
+      int d_bits = 8 - unstuff;
+      tmp        = (tmp << d_bits) | d;
+      bits += d_bits;
+      unstuff = ((d & 0xFF) == 0xFF);
+    }
+    tmp <<= (64 - bits);
+  }
+
+  inline void read() {
+    if (bits > 32) {  // there are enough bits in tmp, return without any reading
+      return;
+    }
+
+    uint32_t val = 0xFFFFFFFF;  // feed in 0xFF if buffer is exhausted
+    if (length > 4) {           // if there is data in the MEL segment
+      val = *reinterpret_cast<uint32_t *>(const_cast<uint8_t *>(buf));  // read 32 bits from MEL data
+      buf += 4;                                                         // advance pointer
+      length -= 4;                                                      // reduce counter
+    } else if (length > 0) {
+      // 4 or less
+      int i = 0;
+      while (length > 1) {
+        uint32_t v = *buf++;                // read one byte at a time
+        uint32_t m = ~(0xFFU << i);         // mask of location
+        val        = (val & m) | (v << i);  // put one byte in its correct location
+        --length;
+        i += 8;
+      }
+      // length equal to 1
+      uint32_t v = *buf++;  // the one before the last is different
+      v |= 0xF;             // MEL and VLC segments may be overlapped
+      uint32_t m = ~(0xFFU << i);
+      val        = (val & m) | (v << i);
+      --length;
+    } else {
+      // error
+    }
+
+    // next we unstuff them before adding them to the buffer
+    int bits_local =
+        32 - unstuff;  // number of bits in val, subtract 1 if the previously read byte requires unstuffing
+
+    // data is unstuffed and accumulated in t
+    // bits_local has the number of bits in t
+    uint32_t t        = val & 0xFF;
+    bool unstuff_flag = ((val & 0xFF) == 0xFF);
+    bits -= unstuff_flag;
+    t = t << (8 - unstuff_flag);
+
+    t |= (val >> 8) & 0xFF;
+    unstuff_flag = (((val >> 8) & 0xFF) == 0xFF);
+    bits -= unstuff_flag;
+    t = t << (8 - unstuff_flag);
+
+    t |= (val >> 16) & 0xFF;
+    unstuff_flag = (((val >> 16) & 0xFF) == 0xFF);
+    bits -= unstuff_flag;
+    t = t << (8 - unstuff_flag);
+
+    t |= (val >> 24) & 0xFF;
+    unstuff_flag = (((val >> 24) & 0xFF) == 0xFF);
+
+    // move to tmp, and push the result all the way up, so we read from the MSB
+    tmp |= (static_cast<uint64_t>(t)) << (64 - bits_local - bits);
+    bits += bits_local;
+  }
+
+  inline void decode() {
+    constexpr int32_t MEL_E[13] = {0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 4, 5};
+    if (bits < 6) {  // if there are less then 6 bits in tmp then read from the MEL bitstream 6 bits that is
+                     // the largest decodable MEL codeword.
+      read();
+    }
+    // repeat so long that there is enough decodable bits in tmp, and the runs store is not full
+    // (num_runs < 8)
+    while (bits >= 6 && num_runs < 8) {
+      int32_t eval = MEL_E[MEL_k];
+      int32_t run  = 0;
+      // The next bit to decode (stored in MSB)
+      if (tmp & (1ULL << 63)) {
+        // "1" is found
+        run = 1 << eval;
+        run--;                                        // consecutive runs of 0 events - 1
+        MEL_k = ((MEL_k + 1) < 12) ? MEL_k + 1 : 12;  // increment, max is 12
+        tmp <<= 1;                                    // consume one bit from tmp
+        bits--;
+        run <<= 1;  // a stretch of zeros not terminating in one
+      } else {
+        // "0" is found
+        run   = static_cast<int32_t>(tmp >> (63 - eval)) & ((1 << eval) - 1);
+        MEL_k = ((MEL_k - 1) > 0) ? MEL_k - 1 : 0;  // decrement, min is 0
+        tmp <<= eval + 1;                           // consume eval + 1 bits (max is 6)
+        bits -= eval + 1;
+        run = (run << 1) + 1;  // a stretch of zeros terminating with one
+      }
+      eval = num_runs * 7;                             // 7 bits per run
+      runs &= ~(static_cast<uint64_t>(0x3F) << eval);  // 6 bits are sufficient
+      runs |= (static_cast<uint64_t>(run)) << eval;    // store the value in runs
+      num_runs++;                                      // increment count
+    }
+  }
+
+  inline int32_t get_run() {
+    if (num_runs == 0) {  // if no runs, decode more bit from MEL segment
+      decode();
+    }
+    int32_t t = runs & 0x7F;  // retrieve one run
+    runs >>= 7;               // remove the retrieved run
+    num_runs--;
+    return t;  // return run
+  }
+};
+
+/********************************************************************************
  * rev_buf:
  *******************************************************************************/
 class rev_buf {
@@ -163,17 +308,17 @@ class rev_buf {
     // 110 2,2,0
     // 111 1,1,0
     // const uint8_t t[8] = {5, 1, 2, 1, 3, 1, 2, 1};
-    const uint8_t t[8] = {3 | (5 << 2), 1 | (1 << 2), 2 | (2 << 2), 1 | (1 << 2),
-                          3 | (3 << 2), 1 | (1 << 2), 2 | (2 << 2), 1 | (1 << 2)};
+    constexpr uint8_t t[8] = {3 | (5 << 2), 1 | (1 << 2), 2 | (2 << 2), 1 | (1 << 2),
+                              3 | (3 << 2), 1 | (1 << 2), 2 | (2 << 2), 1 | (1 << 2)};
     advance(t[val] & 0x3);
     return t[val] >> 2;
   }
 
   inline uint8_t decodeUSuffix(const uint32_t &u_pfx) {
-    const uint8_t ts[6] = {0, 0, 0, 1, 5, 5};
-    uint32_t mask       = static_cast<uint32_t>((1 << ts[u_pfx]) - 1);
-    uint32_t cwd        = fetch();
-    uint8_t val         = static_cast<uint8_t>(cwd & mask);
+    constexpr uint8_t ts[6] = {0, 0, 0, 1, 5, 5};
+    uint32_t mask           = static_cast<uint32_t>((1 << ts[u_pfx]) - 1);
+    uint32_t cwd            = fetch();
+    uint8_t val             = static_cast<uint8_t>(cwd & mask);
     advance(ts[u_pfx]);
     return val;
     //    return (u_pfx < 3) ? 0 : val;
@@ -194,9 +339,9 @@ class rev_buf {
 
   inline uint8_t decodeUExtension(const uint32_t &u_sfx) {
     //    if (u_sfx < 28) return 0;
-    const uint8_t tu[32] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   0,   0,
-                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xF, 0xF, 0xF};
-    uint32_t cwd         = fetch();
+    constexpr uint8_t tu[32] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   0,   0,
+                                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xF, 0xF, 0xF};
+    uint32_t cwd             = fetch();
     advance(4 & tu[u_sfx]);
     return (cwd & tu[u_sfx]);
   }
