6eb408877817c42eda9f4562f5be24c9ef1fafa7
OSAMU WATANABE <owatanab@es.takushoku-u.ac.jp>
2022-06-17 14:00:32 +0900

    Vectorize HT cleanup decoding with ARM NEON

--- source/core/coding/ht_block_decoding.hpp
+++ source/core/coding/ht_block_decoding.hpp
@@ -53,7 +53,7 @@ class MEL_dec {
   uint64_t runs;
 
  public:
-  MEL_dec(const uint8_t *Dcup, uint32_t Lcup, int32_t Scup)
+  MEL_dec(const uint8_t *Dcup, int32_t Lcup, int32_t Scup)
       : bits(0),
         tmp(0),
         buf(Dcup + Lcup - Scup),
@@ -188,7 +188,6 @@ class MEL_dec {
 // this class implementation is borrowed from OpenJPH
 class rev_buf {
  private:
-  int32_t pos;
   uint32_t bits;
   uint64_t Creg;
   uint32_t unstuff;
@@ -196,8 +195,8 @@ class rev_buf {
   int32_t length;
 
  public:
-  rev_buf(uint8_t *Dcup, uint32_t Lcup, int32_t Scup)
-      : pos(Scup - 2), bits(0), Creg(0), unstuff(0), buf(Dcup + Lcup - 2), length(Scup - 2) {
+  rev_buf(uint8_t *Dcup, int32_t Lcup, int32_t Scup)
+      : bits(0), Creg(0), unstuff(0), buf(Dcup + Lcup - 2), length(Scup - 2) {
     uint32_t d = *buf--;  // read a byte (only use it's half byte)
     Creg       = d >> 4;
     bits       = 4 - ((Creg & 0x07) == 0x07);
@@ -455,118 +454,119 @@ class fwd_buf {
 /********************************************************************************
  * state_MS: state class for MagSgn decoding
  *******************************************************************************/
-class state_MS_dec {
- private:
-  uint32_t pos;
-  uint8_t bits;
-  uint8_t tmp;
-  uint8_t last;
-  const uint8_t *buf;
-  const uint32_t length;
-  uint64_t Creg;
-  uint8_t ctreg;
-
- public:
-  state_MS_dec(const uint8_t *Dcup, uint32_t Pcup)
-      : pos(0), bits(0), tmp(0), last(0), buf(Dcup), length(Pcup), Creg(0), ctreg(0) {
-    while (ctreg < 32) {
-      loadByte();
-    }
-  }
-  void loadByte();
-  void close(int32_t num_bits);
-  uint8_t importMagSgnBit();
-  int32_t decodeMagSgnValue(int32_t m_n, int32_t i_n);
-};
-
-/********************************************************************************
- * state_MEL_unPacker and state_MEL: state classes for MEL decoding
- *******************************************************************************/
-class state_MEL_unPacker {
- private:
-  int32_t pos;
-  int8_t bits;
-  uint8_t tmp;
-  const uint8_t *buf;
-  uint32_t length;
-
- public:
-  state_MEL_unPacker(const uint8_t *Dcup, uint32_t Lcup, int32_t Pcup)
-      : pos(Pcup), bits(0), tmp(0), buf(Dcup), length(Lcup) {}
-  uint8_t importMELbit();
-};
-
-class state_MEL_decoder {
- private:
-  uint8_t MEL_k;
-  uint8_t MEL_run;
-  uint8_t MEL_one;
-  const uint8_t MEL_E[13];
-  state_MEL_unPacker *MEL_unPacker;
-
- public:
-  explicit state_MEL_decoder(state_MEL_unPacker &unpacker)
-      : MEL_k(0),
-        MEL_run(0),
-        MEL_one(0),
-        MEL_E{0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 4, 5},
-        MEL_unPacker(&unpacker) {}
-  uint8_t decodeMELSym();
-};
-
-#define ADVANCED
-#ifdef ADVANCED
-  #define getbitfunc getVLCbit()
-#else
-  #define getbitfunc importVLCBit()
-#endif
-/********************************************************************************
- * state_VLC: state class for VLC decoding
- *******************************************************************************/
-class state_VLC_dec {
- private:
-  int32_t pos;
-  uint8_t last;
-#ifndef ADVANCED
-  uint8_t tmp;
-  uint32_t rev_length;
-#else
-  int32_t ctreg;
-  uint64_t Creg;
-#endif
-  uint8_t bits;
-  uint8_t *buf;
-
- public:
-  state_VLC_dec(uint8_t *Dcup, uint32_t Lcup, int32_t Pcup)
-#ifndef ADVANCED
-      : pos((Lcup > 2) ? Lcup - 3 : 0),
-        last(*(Dcup + Lcup - 2)),
-        tmp(last >> 4),
-        rev_length(Pcup),
-        bits(((tmp & 0x07) < 7) ? 4 : 3),
-        buf(Dcup) {
-  }
-  uint8_t importVLCBit();
-#else
-      : pos(static_cast<int32_t>(Lcup) - 2 - Pcup), ctreg(0), Creg(0), bits(0), buf(Dcup + Pcup) {
-    load_bytes();
-    ctreg -= 4;
-    Creg >>= 4;
-    while (ctreg < 32) {
-      load_bytes();
-    }
-  }
-  void load_bytes();
-  uint8_t getVLCbit();
-  void close32(int32_t num_bits);
-#endif
-  void decodeCxtVLC(const uint16_t &context, uint8_t (&u_off)[2], uint8_t (&rho)[2], uint8_t (&emb_k)[2],
-                    uint8_t (&emb_1)[2], const uint8_t &first_or_second, const uint16_t *dec_CxtVLC_table);
-  uint8_t decodeUPrefix();
-  uint8_t decodeUSuffix(const uint32_t &u_pfx);
-  uint8_t decodeUExtension(const uint32_t &u_sfx);
-};
+// class state_MS_dec {
+//  private:
+//   uint32_t pos;
+//   uint8_t bits;
+//   uint8_t tmp;
+//   uint8_t last;
+//   const uint8_t *buf;
+//   const uint32_t length;
+//   uint64_t Creg;
+//   uint8_t ctreg;
+//
+//  public:
+//   state_MS_dec(const uint8_t *Dcup, uint32_t Pcup)
+//       : pos(0), bits(0), tmp(0), last(0), buf(Dcup), length(Pcup), Creg(0), ctreg(0) {
+//     while (ctreg < 32) {
+//       loadByte();
+//     }
+//   }
+//   void loadByte();
+//   void close(int32_t num_bits);
+//   uint8_t importMagSgnBit();
+//   int32_t decodeMagSgnValue(int32_t m_n, int32_t i_n);
+// };
+//
+///********************************************************************************
+// * state_MEL_unPacker and state_MEL: state classes for MEL decoding
+// *******************************************************************************/
+// class state_MEL_unPacker {
+// private:
+//  int32_t pos;
+//  int8_t bits;
+//  uint8_t tmp;
+//  const uint8_t *buf;
+//  uint32_t length;
+//
+// public:
+//  state_MEL_unPacker(const uint8_t *Dcup, uint32_t Lcup, int32_t Pcup)
+//      : pos(Pcup), bits(0), tmp(0), buf(Dcup), length(Lcup) {}
+//  uint8_t importMELbit();
+//};
+//
+// class state_MEL_decoder {
+// private:
+//  uint8_t MEL_k;
+//  uint8_t MEL_run;
+//  uint8_t MEL_one;
+//  const uint8_t MEL_E[13];
+//  state_MEL_unPacker *MEL_unPacker;
+//
+// public:
+//  explicit state_MEL_decoder(state_MEL_unPacker &unpacker)
+//      : MEL_k(0),
+//        MEL_run(0),
+//        MEL_one(0),
+//        MEL_E{0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 4, 5},
+//        MEL_unPacker(&unpacker) {}
+//  uint8_t decodeMELSym();
+//};
+//
+//#define ADVANCED
+//#ifdef ADVANCED
+//  #define getbitfunc getVLCbit()
+//#else
+//  #define getbitfunc importVLCBit()
+//#endif
+///********************************************************************************
+// * state_VLC: state class for VLC decoding
+// *******************************************************************************/
+// class state_VLC_dec {
+// private:
+//  int32_t pos;
+//  uint8_t last;
+//#ifndef ADVANCED
+//  uint8_t tmp;
+//  uint32_t rev_length;
+//#else
+//  int32_t ctreg;
+//  uint64_t Creg;
+//#endif
+//  uint8_t bits;
+//  uint8_t *buf;
+//
+// public:
+//  state_VLC_dec(uint8_t *Dcup, uint32_t Lcup, int32_t Pcup)
+//#ifndef ADVANCED
+//      : pos((Lcup > 2) ? Lcup - 3 : 0),
+//        last(*(Dcup + Lcup - 2)),
+//        tmp(last >> 4),
+//        rev_length(Pcup),
+//        bits(((tmp & 0x07) < 7) ? 4 : 3),
+//        buf(Dcup) {
+//  }
+//  uint8_t importVLCBit();
+//#else
+//      : pos(static_cast<int32_t>(Lcup) - 2 - Pcup), ctreg(0), Creg(0), bits(0), buf(Dcup + Pcup) {
+//    load_bytes();
+//    ctreg -= 4;
+//    Creg >>= 4;
+//    while (ctreg < 32) {
+//      load_bytes();
+//    }
+//  }
+//  void load_bytes();
+//  uint8_t getVLCbit();
+//  void close32(int32_t num_bits);
+//#endif
+//  void decodeCxtVLC(const uint16_t &context, uint8_t (&u_off)[2], uint8_t (&rho)[2], uint8_t (&emb_k)[2],
+//                    uint8_t (&emb_1)[2], const uint8_t &first_or_second, const uint16_t
+//                    *dec_CxtVLC_table);
+//  uint8_t decodeUPrefix();
+//  uint8_t decodeUSuffix(const uint32_t &u_pfx);
+//  uint8_t decodeUExtension(const uint32_t &u_sfx);
+//};
 /********************************************************************************
  * SP_dec: state class for HT SigProp decoding
  *******************************************************************************/
