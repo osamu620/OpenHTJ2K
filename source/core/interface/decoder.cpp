c45cb25d083b1d4d797639ca04f99a2987e41106
OSAMU WATANABE <owatanab@es.takushoku-u.ac.jp>
2022-07-19 16:45:26 +0900

    Small improvements in `openhtj2k_decoder_impl::invoke()`

--- source/core/interface/decoder.cpp
+++ source/core/interface/decoder.cpp
@@ -164,8 +164,10 @@ void openhtj2k_decoder_impl::invoke(std::vector<int32_t *> &buf, std::vector<uin
 
   // prepare output buffer
   for (uint16_t c = 0; c < num_components; c++) {
+    int32_t *dp = buf[c];
     for (uint32_t i = 0; i < numTiles.x * numTiles.y; i++) {
       j2k_tile_component *tile_component = tileSet[i].get_tile_component(c);
+      int32_t *sp                        = tile_component->get_sample_address(0, 0);
       element_siz component_pos          = tile_component->get_pos0();
       element_siz component_size;
       tile_component->get_size(component_size);
@@ -173,9 +175,9 @@ void openhtj2k_decoder_impl::invoke(std::vector<int32_t *> &buf, std::vector<uin
       uint32_t component_height = component_size.y;
       uint32_t x_offset         = component_pos.x - ceil_int(x0[c], (1U << reduce_NL));
       uint32_t y_offset         = component_pos.y - ceil_int(y0[c], (1U << reduce_NL));
-      for (uint32_t y = 0; y < component_height; y++) {
-        int32_t *sp = tile_component->get_sample_address(0, y);
-        memcpy(&buf[c][(0 + x_offset) + (y + y_offset) * width[c]], sp, sizeof(int32_t) * component_width);
+      for (uint32_t y = y_offset; y < component_height + y_offset; y++) {
+        memcpy(dp + x_offset + y * width[c], sp, sizeof(int32_t) * component_width);
+        sp += component_width;
       }
     }
   }
