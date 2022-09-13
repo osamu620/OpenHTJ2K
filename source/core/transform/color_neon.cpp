ba2d2d5719505660d33729a90f73a323498d2333
OSAMU WATANABE <owatanab@es.takushoku-u.ac.jp>
2022-09-13 17:57:28 +0900

    Improve color conversion for NEON

--- source/core/transform/color_neon.cpp
+++ source/core/transform/color_neon.cpp
@@ -33,19 +33,28 @@
 void cvt_rgb_to_ycbcr_rev_neon(int32_t *sp0, int32_t *sp1, int32_t *sp2, uint32_t num_tc_samples) {
   int32_t R, G, B;
   int32_t Y, Cb, Cr;
-  for (; num_tc_samples > 4; num_tc_samples -= 4) {
-    auto vR  = vld1q_s32(sp0);
-    auto vG  = vld1q_s32(sp1);
-    auto vB  = vld1q_s32(sp2);
-    auto vY  = (vR + 2 * vG + vB) >> 2;
-    auto vCb = vB - vG;
-    auto vCr = vR - vG;
-    vst1q_s32(sp0, vY);
-    vst1q_s32(sp1, vCb);
-    vst1q_s32(sp2, vCr);
-    sp0 += 4;
-    sp1 += 4;
-    sp2 += 4;
+  for (; num_tc_samples >= 8; num_tc_samples -= 8) {
+    auto vR0  = vld1q_s32(sp0);
+    auto vR1  = vld1q_s32(sp0 + 4);
+    auto vG0  = vld1q_s32(sp1);
+    auto vG1  = vld1q_s32(sp1 + 4);
+    auto vB0  = vld1q_s32(sp2);
+    auto vB1  = vld1q_s32(sp2 + 4);
+    auto vY0  = (vR0 + 2 * vG0 + vB0) >> 2;
+    auto vY1  = (vR1 + 2 * vG1 + vB1) >> 2;
+    auto vCb0 = vB0 - vG0;
+    auto vCb1 = vB1 - vG1;
+    auto vCr0 = vR0 - vG0;
+    auto vCr1 = vR1 - vG1;
+    vst1q_s32(sp0, vY0);
+    vst1q_s32(sp0 + 4, vY1);
+    vst1q_s32(sp1, vCb0);
+    vst1q_s32(sp1 + 4, vCb1);
+    vst1q_s32(sp2, vCr0);
+    vst1q_s32(sp2 + 4, vCr1);
+    sp0 += 8;
+    sp1 += 8;
+    sp2 += 8;
   }
   for (; num_tc_samples > 0; --num_tc_samples) {
     R      = *sp0;
@@ -63,32 +72,46 @@ void cvt_rgb_to_ycbcr_rev_neon(int32_t *sp0, int32_t *sp1, int32_t *sp2, uint32_
 void cvt_rgb_to_ycbcr_irrev_neon(int32_t *sp0, int32_t *sp1, int32_t *sp2, uint32_t num_tc_samples) {
   double fRed, fGrn, fBlu;
   double fY, fCb, fCr;
-  int32x4_t R, G, B;
-  float32x4_t Y, Cb, Cr, fR, fG, fB;
+  int32x4_t R0, G0, B0, R1, G1, B1;
+  float32x4_t Y0, Cb0, Cr0, fR0, fG0, fB0, Y1, Cb1, Cr1, fR1, fG1, fB1;
   float32x4_t a0 = vdupq_n_f32(static_cast<float32_t>(ALPHA_R));
   float32x4_t a1 = vdupq_n_f32(static_cast<float32_t>(ALPHA_G));
   float32x4_t a2 = vdupq_n_f32(static_cast<float32_t>(ALPHA_B));
-  for (; num_tc_samples >= 4; num_tc_samples -= 4) {
-    R  = vld1q_s32(sp0);
-    G  = vld1q_s32(sp1);
-    B  = vld1q_s32(sp2);
-    fR = vcvtq_f32_s32(R);
-    fG = vcvtq_f32_s32(G);
-    fB = vcvtq_f32_s32(B);
-    Y  = fR * a0;
-    Y  = vmlaq_f32(Y, fG, a1);
-    Y  = vmlaq_f32(Y, fB, a2);
-    // Y  = fR * a0 + fG * a1 + fB * a2;
-    Cb = vmulq_n_f32(vsubq_f32(fB, Y), static_cast<float32_t>(1.0 / CB_FACT_B));
-    Cr = vmulq_n_f32(vsubq_f32(fR, Y), static_cast<float32_t>(1.0 / CR_FACT_R));
+  for (; num_tc_samples >= 8; num_tc_samples -= 8) {
+    R0  = vld1q_s32(sp0);
+    R1  = vld1q_s32(sp0 + 4);
+    G0  = vld1q_s32(sp1);
+    G1  = vld1q_s32(sp1 + 4);
+    B0  = vld1q_s32(sp2);
+    B1  = vld1q_s32(sp2 + 4);
+    fR0 = vcvtq_f32_s32(R0);
+    fR1 = vcvtq_f32_s32(R1);
+    fG0 = vcvtq_f32_s32(G0);
+    fG1 = vcvtq_f32_s32(G1);
+    fB0 = vcvtq_f32_s32(B0);
+    fB1 = vcvtq_f32_s32(B1);
+    Y0  = vmulq_f32(fR0, a0);
+    Y0  = vfmaq_f32(Y0, fG0, a1);
+    Y0  = vfmaq_f32(Y0, fB0, a2);
+    Y1  = vmulq_f32(fR1, a0);
+    Y1  = vfmaq_f32(Y1, fG1, a1);
+    Y1  = vfmaq_f32(Y1, fB1, a2);
+    // Y0  = fR0 * a0 + fG0 * a1 + fB0 * a2;
+    Cb0 = vmulq_n_f32(vsubq_f32(fB0, Y0), static_cast<float32_t>(1.0 / CB_FACT_B));
+    Cb1 = vmulq_n_f32(vsubq_f32(fB1, Y1), static_cast<float32_t>(1.0 / CB_FACT_B));
+    Cr0 = vmulq_n_f32(vsubq_f32(fR0, Y0), static_cast<float32_t>(1.0 / CR_FACT_R));
+    Cr1 = vmulq_n_f32(vsubq_f32(fR1, Y1), static_cast<float32_t>(1.0 / CR_FACT_R));
 
     // TODO: need to consider precision and setting FPSCR register value
-    vst1q_s32(sp0, vcvtnq_s32_f32(Y));
-    vst1q_s32(sp1, vcvtnq_s32_f32(Cb));
-    vst1q_s32(sp2, vcvtnq_s32_f32(Cr));
-    sp0 += 4;
-    sp1 += 4;
-    sp2 += 4;
+    vst1q_s32(sp0, vcvtnq_s32_f32(Y0));
+    vst1q_s32(sp0 + 4, vcvtnq_s32_f32(Y1));
+    vst1q_s32(sp1, vcvtnq_s32_f32(Cb0));
+    vst1q_s32(sp1 + 4, vcvtnq_s32_f32(Cb1));
+    vst1q_s32(sp2, vcvtnq_s32_f32(Cr0));
+    vst1q_s32(sp2 + 4, vcvtnq_s32_f32(Cr1));
+    sp0 += 8;
+    sp1 += 8;
+    sp2 += 8;
   }
 
   for (; num_tc_samples > 0; --num_tc_samples) {
@@ -157,20 +180,30 @@ void cvt_ycbcr_to_rgb_rev_neon(int32_t *sp0, int32_t *sp1, int32_t *sp2, uint32_
 void cvt_ycbcr_to_rgb_irrev_neon(int32_t *sp0, int32_t *sp1, int32_t *sp2, uint32_t num_tc_samples) {
   int32_t R, G, B;
   double fY, fCb, fCr;
-  for (; num_tc_samples >= 4; num_tc_samples -= 4) {
-    auto vY  = vcvtq_f32_s32(vld1q_s32(sp0));
-    auto vCb = vcvtq_f32_s32(vld1q_s32(sp1));
-    auto vCr = vcvtq_f32_s32(vld1q_s32(sp2));
+  const float32x4_t fCR_FACT_R = vdupq_n_f32(static_cast<float32_t>(CR_FACT_R));
+  const float32x4_t fCB_FACT_B = vdupq_n_f32(static_cast<float32_t>(CB_FACT_B));
+  const float32x4_t fCR_FACT_G = vdupq_n_f32(static_cast<float32_t>(CR_FACT_G));
+  const float32x4_t fCB_FACT_G = vdupq_n_f32(static_cast<float32_t>(CB_FACT_G));
+  for (; num_tc_samples >= 8; num_tc_samples -= 8) {
+    auto Y0  = vcvtq_f32_s32(vld1q_s32(sp0));
+    auto Y1  = vcvtq_f32_s32(vld1q_s32(sp0 + 4));
+    auto Cb0 = vcvtq_f32_s32(vld1q_s32(sp1));
+    auto Cb1 = vcvtq_f32_s32(vld1q_s32(sp1 + 4));
+    auto Cr0 = vcvtq_f32_s32(vld1q_s32(sp2));
+    auto Cr1 = vcvtq_f32_s32(vld1q_s32(sp2 + 4));
 
     // TODO: need to consider precision and setting FPSCR register value
-    vst1q_s32(sp0, vcvtnq_s32_f32(vaddq_f32(vY, vmulq_n_f32(vCr, static_cast<float32_t>(CR_FACT_R)))));
-    vst1q_s32(sp2, vcvtnq_s32_f32(vaddq_f32(vY, vmulq_n_f32(vCb, static_cast<float32_t>(CB_FACT_B)))));
-    vst1q_s32(
-        sp1, vcvtnq_s32_f32(vsubq_f32(vY, vaddq_f32(vmulq_n_f32(vCr, static_cast<float32_t>(CR_FACT_G)),
-                                                    vmulq_n_f32(vCb, static_cast<float32_t>(CB_FACT_G))))));
-    sp0 += 4;
-    sp1 += 4;
-    sp2 += 4;
+    vst1q_s32(sp0, vcvtnq_s32_f32(vfmaq_f32(Y0, Cr0, fCR_FACT_R)));
+    vst1q_s32(sp0 + 4, vcvtnq_s32_f32(vfmaq_f32(Y1, Cr1, fCR_FACT_R)));
+    vst1q_s32(sp2, vcvtnq_s32_f32(vfmaq_f32(Y0, Cb0, fCB_FACT_B)));
+    vst1q_s32(sp2 + 4, vcvtnq_s32_f32(vfmaq_f32(Y1, Cb1, fCB_FACT_B)));
+    vst1q_s32(sp1, vcvtnq_s32_f32(
+                       vsubq_f32(Y0, vaddq_f32(vmulq_f32(Cr0, fCR_FACT_G), vmulq_f32(Cb0, fCB_FACT_G)))));
+    vst1q_s32(sp1 + 4, vcvtnq_s32_f32(vsubq_f32(
+                           Y1, vaddq_f32(vmulq_f32(Cr1, fCR_FACT_G), vmulq_f32(Cb1, fCB_FACT_G)))));
+    sp0 += 8;
+    sp1 += 8;
+    sp2 += 8;
   }
   for (; num_tc_samples > 0; --num_tc_samples) {
     fY     = static_cast<double>(*sp0);
