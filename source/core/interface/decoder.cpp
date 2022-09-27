b3d1bb346962b71d42be1d10ccfa3bf2adde8c18
OSAMU WATANABE <owatanab@es.takushoku-u.ac.jp>
2022-09-27 12:19:56 +0900

    Code cleanup

--- source/core/interface/decoder.cpp
+++ source/core/interface/decoder.cpp
@@ -183,12 +183,10 @@ void openhtj2k_decoder_impl::invoke(std::vector<int32_t *> &buf, std::vector<uin
 }
 
 openhtj2k_decoder_impl::~openhtj2k_decoder_impl() {
-#ifdef OPENHTJ2K_THREAD
-  auto pool = ThreadPool::get();
-  if (pool->num_threads() > 1) {
-    ThreadPool::release();
-  }
-#endif
+  //#ifdef OPENHTJ2K_THREAD
+  //  auto pool = ThreadPool::get();
+  //  pool->release();
+  //#endif
 }
 
 // public interface
