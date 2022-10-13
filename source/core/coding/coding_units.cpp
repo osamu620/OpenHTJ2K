832327656de27f297b079ecbceef2c132604107f
OSAMU WATANABE <owatanab@es.takushoku-u.ac.jp>
2022-10-13 14:17:20 +0900

    Fix possible infinite loop

--- source/core/coding/coding_units.cpp
+++ source/core/coding/coding_units.cpp
@@ -243,9 +243,10 @@ void j2k_codeblock::create_compressed_buffer(buf_chain *tile_buf, int32_t buf_li
       while (this->length + layer_length > static_cast<uint32_t>(buf_limit)) {
         // extend buffer size, if necessary
         uint8_t *newbuf =
-            static_cast<uint8_t *>(realloc(this->compressed_data, static_cast<size_t>(buf_limit)));
+            static_cast<uint8_t *>(realloc(this->compressed_data, this->length + layer_length));
         this->compressed_data = newbuf;
         this->current_address = this->compressed_data + (this->length);
+        buf_limit             = this->length + layer_length;
         //        uint8_t *old_buf      = this->compressed_data.release();
         //        buf_limit += 8192;
         //        this->compressed_data = std::unique_ptr<uint8_t[]>(new uint8_t[static_cast<uint32_t>(
