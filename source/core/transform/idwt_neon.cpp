1faab865718ea08b35d49dd0d1f09bf4a354b872
OSAMU WATANABE <owatanab@es.takushoku-u.ac.jp>
2022-10-24 10:53:23 +0900

    Fix undefined behavior

--- source/core/transform/idwt_neon.cpp
+++ source/core/transform/idwt_neon.cpp
@@ -371,13 +371,16 @@ void idwt_rev_ver_sr_fixed_neon(sprec_t *in, const int32_t u0, const int32_t u1,
       int16_t *x0 = buf[n - 1];
       int16_t *x1 = buf[n];
       int16_t *x2 = buf[n + 1];
-      vin00       = vld1q_s16(x0);
-      vin01       = vld1q_s16(x0 + 8);
-      vout0       = vld1q_s16(x1);
-      vout1       = vld1q_s16(x1 + 8);
-      vin10       = vld1q_s16(x2);
-      vin11       = vld1q_s16(x2 + 8);
+      __builtin_prefetch(x0);
+      __builtin_prefetch(x1);
+      __builtin_prefetch(x2);
       for (int32_t col = 0; col < simdlen; col += 16) {
+        vin00 = vld1q_s16(x0);
+        vin01 = vld1q_s16(x0 + 8);
+        vout0 = vld1q_s16(x1);
+        vout1 = vld1q_s16(x1 + 8);
+        vin10 = vld1q_s16(x2);
+        vin11 = vld1q_s16(x2 + 8);
         vout0 -= vrshrq_n_s16(vhaddq_s16(vin00, vin10), 1);
         vout1 -= vrshrq_n_s16(vhaddq_s16(vin01, vin11), 1);
         vst1q_s16(x1, vout0);
@@ -385,12 +388,9 @@ void idwt_rev_ver_sr_fixed_neon(sprec_t *in, const int32_t u0, const int32_t u1,
         x0 += 16;
         x1 += 16;
         x2 += 16;
-        vin00 = vld1q_s16(x0);
-        vin01 = vld1q_s16(x0 + 8);
-        vout0 = vld1q_s16(x1);
-        vout1 = vld1q_s16(x1 + 8);
-        vin10 = vld1q_s16(x2);
-        vin11 = vld1q_s16(x2 + 8);
+        __builtin_prefetch(x0);
+        __builtin_prefetch(x1);
+        __builtin_prefetch(x2);
       }
       for (int32_t col = simdlen; col < u1 - u0; ++col) {
         int32_t sum = *x0++;
@@ -402,13 +402,16 @@ void idwt_rev_ver_sr_fixed_neon(sprec_t *in, const int32_t u0, const int32_t u1,
       int16_t *x0 = buf[n];
       int16_t *x1 = buf[n + 1];
       int16_t *x2 = buf[n + 2];
-      vin00       = vld1q_s16(x0);
-      vin01       = vld1q_s16(x0 + 8);
-      vout0       = vld1q_s16(x1);
-      vout1       = vld1q_s16(x1 + 8);
-      vin10       = vld1q_s16(x2);
-      vin11       = vld1q_s16(x2 + 8);
+      __builtin_prefetch(x0);
+      __builtin_prefetch(x1);
+      __builtin_prefetch(x2);
       for (int32_t col = 0; col < simdlen; col += 16) {
+        vin00 = vld1q_s16(x0);
+        vin01 = vld1q_s16(x0 + 8);
+        vout0 = vld1q_s16(x1);
+        vout1 = vld1q_s16(x1 + 8);
+        vin10 = vld1q_s16(x2);
+        vin11 = vld1q_s16(x2 + 8);
         vout0 += vhaddq_s16(vin00, vin10);
         vout1 += vhaddq_s16(vin01, vin11);
         vst1q_s16(x1, vout0);
@@ -416,12 +419,9 @@ void idwt_rev_ver_sr_fixed_neon(sprec_t *in, const int32_t u0, const int32_t u1,
         x0 += 16;
         x1 += 16;
         x2 += 16;
-        vin00 = vld1q_s16(x0);
-        vin01 = vld1q_s16(x0 + 8);
-        vout0 = vld1q_s16(x1);
-        vout1 = vld1q_s16(x1 + 8);
-        vin10 = vld1q_s16(x2);
-        vin11 = vld1q_s16(x2 + 8);
+        __builtin_prefetch(x0);
+        __builtin_prefetch(x1);
+        __builtin_prefetch(x2);
       }
       for (int32_t col = simdlen; col < u1 - u0; ++col) {
         int32_t sum = *x0++;
