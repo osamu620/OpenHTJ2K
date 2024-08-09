e366d28a061a037d7de28e1f6426e4a7ab495e63
OSAMU WATANABE <owatanab@es.takushoku-u.ac.jp>
2024-08-09 15:57:22 +0900

    Add alternative decoding logic

--- source/core/coding/ht_block_decoding.cpp
+++ source/core/coding/ht_block_decoding.cpp
@@ -53,6 +53,422 @@ uint8_t j2k_codeblock::calc_mbr(const uint32_t i, const uint32_t j, const uint8_
 
 void ht_cleanup_decode(j2k_codeblock *block, const uint8_t &pLSB, const int32_t Lcup, const int32_t Pcup,
                        const int32_t Scup) {
+  uint8_t *compressed_data = block->get_compressed_data();
+  const uint16_t QW        = static_cast<uint16_t>(ceil_int(static_cast<int16_t>(block->size.x), 2));
+  const uint16_t QH        = static_cast<uint16_t>(ceil_int(static_cast<int16_t>(block->size.y), 2));
+
+  uint16_t scratch[8 * 513] = {0};
+  int32_t sstr              = static_cast<int32_t>(((block->size.x + 2) + 7u) & ~7u);  // multiples of 8
+  uint16_t *sp;
+  int32_t qx;
+  /*******************************************************************************************************************/
+  // VLC, UVLC and MEL decoding
+  /*******************************************************************************************************************/
+  MEL_dec MEL(compressed_data, Lcup, Scup);
+  rev_buf VLC_dec(compressed_data, Lcup, Scup);
+  auto sp0 = block->block_states + 1 + block->blkstate_stride;
+  auto sp1 = block->block_states + 1 + 2 * block->blkstate_stride;
+  uint32_t u_off0, u_off1;
+  uint32_t u0, u1;
+  uint32_t context = 0;
+  uint32_t vlcval;
+
+  const uint16_t *dec_table;
+  // Initial line-pair
+  dec_table       = dec_CxtVLC_table0_fast_16;
+  sp              = scratch;
+  int32_t mel_run = MEL.get_run();
+  for (qx = QW; qx > 0; qx -= 2, sp += 4) {
+    // Decoding of significance and EMB patterns and unsigned residual offsets
+    vlcval       = VLC_dec.fetch();
+    uint16_t tv0 = dec_table[(vlcval & 0x7F) + context];
+    if (context == 0) {
+      mel_run -= 2;
+      tv0 = (mel_run == -1) ? tv0 : 0;
+      if (mel_run < 0) {
+        mel_run = MEL.get_run();
+      }
+    }
+    sp[0] = tv0;
+
+    // calculate context for the next quad, Eq. (1) in the spec
+    context = ((tv0 & 0xE0U) << 2) | ((tv0 & 0x10U) << 3);  // = context << 7
+
+    // Decoding of significance and EMB patterns and unsigned residual offsets
+    vlcval       = VLC_dec.advance((tv0 & 0x000F) >> 1);
+    uint16_t tv1 = dec_table[(vlcval & 0x7F) + context];
+    if (context == 0 && qx > 1) {
+      mel_run -= 2;
+      tv1 = (mel_run == -1) ? tv1 : 0;
+      if (mel_run < 0) {
+        mel_run = MEL.get_run();
+      }
+    }
+    tv1   = (qx > 1) ? tv1 : 0;
+    sp[2] = tv1;
+
+    // store sigma
+    *sp0++ = ((tv0 >> 4) >> 0) & 1;
+    *sp0++ = ((tv0 >> 4) >> 2) & 1;
+    *sp0++ = ((tv1 >> 4) >> 0) & 1;
+    *sp0++ = ((tv1 >> 4) >> 2) & 1;
+    *sp1++ = ((tv0 >> 4) >> 1) & 1;
+    *sp1++ = ((tv0 >> 4) >> 3) & 1;
+    *sp1++ = ((tv1 >> 4) >> 1) & 1;
+    *sp1++ = ((tv1 >> 4) >> 3) & 1;
+
+    // calculate context for the next quad, Eq. (1) in the spec
+    context = ((tv1 & 0xE0U) << 2) | ((tv1 & 0x10U) << 3);  // = context << 7
+
+    vlcval = VLC_dec.advance((tv1 & 0x000F) >> 1);
+    u_off0 = tv0 & 1;
+    u_off1 = tv1 & 1;
+
+    uint32_t mel_offset = 0;
+    if (u_off0 == 1 && u_off1 == 1) {
+      mel_run -= 2;
+      mel_offset = (mel_run == -1) ? 0x40 : 0;
+      if (mel_run < 0) {
+        mel_run = MEL.get_run();
+      }
+    }
+
+    // UVLC decoding
+    uint32_t idx         = (vlcval & 0x3F) + (u_off0 << 6U) + (u_off1 << 7U) + mel_offset;
+    uint32_t uvlc_result = uvlc_dec_0[idx];
+    // remove total prefix length
+    vlcval = VLC_dec.advance(uvlc_result & 0x7);
+    uvlc_result >>= 3;
+    // extract suffixes for quad 0 and 1
+    uint32_t len = uvlc_result & 0xF;  // suffix length for 2 quads (up to 10 = 5 + 5)
+    //  ((1U << len) - 1U) can be replaced with _bzhi_u32(UINT32_MAX, len); not fast
+    uint32_t tmp = vlcval & ((1U << len) - 1U);  // suffix value for 2 quads
+    vlcval       = VLC_dec.advance(len);
+    uvlc_result >>= 4;
+    // quad 0 length
+    len = uvlc_result & 0x7;  // quad 0 suffix length
+    uvlc_result >>= 3;
+    // U = 1+ u
+    u0 = 1 + (uvlc_result & 7) + (tmp & ~(0xFFU << len));  // always kappa = 1 in initial line pair
+    u1 = 1 + (uvlc_result >> 3) + (tmp >> len);            // always kappa = 1 in initial line pair
+
+    sp[1] = static_cast<uint16_t>(u0);
+    sp[3] = static_cast<uint16_t>(u1);
+  }
+  // sp[0] = sp[1] = 0;
+
+  // Non-initial line-pair
+  dec_table = dec_CxtVLC_table1_fast_16;
+  for (uint16_t row = 1; row < QH; row++) {
+    sp0 = block->block_states + (row * 2U + 1U) * block->blkstate_stride + 1U;
+    sp1 = sp0 + block->blkstate_stride;
+
+    sp = scratch + row * sstr;
+    // calculate context for the next quad: w, sw, nw are always 0 at the head of a row
+    context = ((sp[0 - sstr] & 0xA0U) << 2) | ((sp[2 - sstr] & 0x20U) << 4);
+    for (qx = QW; qx > 0; qx -= 2, sp += 4) {
+      // Decoding of significance and EMB patterns and unsigned residual offsets
+      vlcval       = VLC_dec.fetch();
+      uint16_t tv0 = dec_table[(vlcval & 0x7F) + context];
+      if (context == 0) {
+        mel_run -= 2;
+        tv0 = (mel_run == -1) ? tv0 : 0;
+        if (mel_run < 0) {
+          mel_run = MEL.get_run();
+        }
+      }
+      // calculate context for the next quad, Eq. (2) in the spec
+      context = ((tv0 & 0x40U) << 2) | ((tv0 & 0x80U) << 1);              // (w | sw) << 8
+      context |= (sp[0 - sstr] & 0x80U) | ((sp[2 - sstr] & 0xA0U) << 2);  // ((nw | n) << 7) | (ne << 9)
+      context |= (sp[4 - sstr] & 0x20U) << 4;                             // ( nf) << 9
+
+      sp[0] = tv0;
+
+      vlcval = VLC_dec.advance((tv0 & 0x000F) >> 1);
+
+      // Decoding of significance and EMB patterns and unsigned residual offsets
+      uint16_t tv1 = dec_table[(vlcval & 0x7F) + context];
+      if (context == 0 && qx > 1) {
+        mel_run -= 2;
+        tv1 = (mel_run == -1) ? tv1 : 0;
+        if (mel_run < 0) {
+          mel_run = MEL.get_run();
+        }
+      }
+      tv1 = (qx > 1) ? tv1 : 0;
+      // calculate context for the next quad, Eq. (2) in the spec
+      context = ((tv1 & 0x40U) << 2) | ((tv1 & 0x80U) << 1);              // (w | sw) << 8
+      context |= (sp[2 - sstr] & 0x80U) | ((sp[4 - sstr] & 0xA0U) << 2);  // ((nw | n) << 7) | (ne << 9)
+      context |= (sp[6 - sstr] & 0x20U) << 4;                             // ( nf) << 9
+
+      sp[2] = tv1;
+
+      // store sigma
+      *sp0++ = ((tv0 >> 4) >> 0) & 1;
+      *sp0++ = ((tv0 >> 4) >> 2) & 1;
+      *sp0++ = ((tv1 >> 4) >> 0) & 1;
+      *sp0++ = ((tv1 >> 4) >> 2) & 1;
+      *sp1++ = ((tv0 >> 4) >> 1) & 1;
+      *sp1++ = ((tv0 >> 4) >> 3) & 1;
+      *sp1++ = ((tv1 >> 4) >> 1) & 1;
+      *sp1++ = ((tv1 >> 4) >> 3) & 1;
+
+      vlcval = VLC_dec.advance((tv1 & 0x000F) >> 1);
+
+      // UVLC decoding
+      u_off0       = tv0 & 1;
+      u_off1       = tv1 & 1;
+      uint32_t idx = (vlcval & 0x3F) + (u_off0 << 6U) + (u_off1 << 7U);
+
+      uint32_t uvlc_result = uvlc_dec_1[idx];
+      // remove total prefix length
+      vlcval = VLC_dec.advance(uvlc_result & 0x7);
+      uvlc_result >>= 3;
+      // extract suffixes for quad 0 and 1
+      uint32_t len = uvlc_result & 0xF;  // suffix length for 2 quads (up to 10 = 5 + 5)
+      //  ((1U << len) - 1U) can be replaced with _bzhi_u32(UINT32_MAX, len); not fast
+      uint32_t tmp = vlcval & ((1U << len) - 1U);  // suffix value for 2 quads
+      vlcval       = VLC_dec.advance(len);
+      uvlc_result >>= 4;
+      // quad 0 length
+      len = uvlc_result & 0x7;  // quad 0 suffix length
+      uvlc_result >>= 3;
+      u0 = (uvlc_result & 7) + (tmp & ~(0xFFU << len));
+      u1 = (uvlc_result >> 3) + (tmp >> len);
+
+      sp[1] = static_cast<uint16_t>(u0);
+      sp[3] = static_cast<uint16_t>(u1);
+    }
+    // sp[0] = sp[1] = 0;
+  }
+
+  /*******************************************************************************************************************/
+  // MagSgn decoding
+  /*******************************************************************************************************************/
+  {
+    // We allocate a scratch row for storing v_n values.
+    // We have 512 quads horizontally.
+    // We need an extra entry to handle the case of vp[1]
+    // when vp is at the last column.
+    // Here, we allocate 4 instead of 1 to make the buffer size
+    // a multipled of 16 bytes.
+    const int v_n_size             = 512 + 4;
+    uint32_t v_n_scratch[v_n_size] = {0};  // 2+ kB
+
+    fwd_buf<0xFF> MagSgn(compressed_data, Pcup);
+
+    uint16_t *sp = scratch;
+    uint32_t *vp = v_n_scratch;
+    int32_t *dp  = block->sample_buf;
+
+    uint32_t prev_v_n = 0;
+    for (uint32_t x = 0; x < block->size.x; sp += 2, ++vp) {
+      uint32_t inf = sp[0];
+      uint32_t U_q = sp[1];
+      if (U_q > ((30 - pLSB) + 2)) {
+        printf("ERROR\n");
+      }
+
+      uint32_t v_n;
+      uint32_t val = 0;
+      uint32_t bit = 0;
+      if (inf & (1 << (4 + bit))) {
+        // get 32 bits of magsgn data
+        uint32_t ms_val = MagSgn.fetch();
+        uint32_t m_n    = U_q - ((inf >> (12 + bit)) & 1);  // remove e_k
+        MagSgn.advance(m_n);                                // consume m_n
+
+        val = ms_val << 31;                      // get sign bit
+        v_n = ms_val & ((1 << m_n) - 1);         // keep only m_n bits
+        v_n |= ((inf >> (8 + bit)) & 1) << m_n;  // add EMB e_1 as MSB
+        v_n |= 1;                                // add center of bin
+        // v_n now has 2 * (\mu - 1) + 0.5 with correct sign bit
+        // add 2 to make it 2*\mu+0.5, shift it up to missing MSBs
+        val |= (v_n + 2) << (pLSB - 1);
+      }
+      dp[0] = val;
+
+      v_n = 0;
+      val = 0;
+      bit = 1;
+      if (inf & (1 << (4 + bit))) {
+        // get 32 bits of magsgn data
+        uint32_t ms_val = MagSgn.fetch();
+        uint32_t m_n    = U_q - ((inf >> (12 + bit)) & 1);  // remove e_k
+        MagSgn.advance(m_n);                                // consume m_n
+
+        val = ms_val << 31;                      // get sign bit
+        v_n = ms_val & ((1 << m_n) - 1);         // keep only m_n bits
+        v_n |= ((inf >> (8 + bit)) & 1) << m_n;  // add EMB e_1 as MSB
+        v_n |= 1;                                // add center of bin
+        // v_n now has 2 * (\mu - 1) + 0.5 with correct sign bit
+        // add 2 to make it 2*\mu+0.5, shift it up to missing MSBs
+        val |= (v_n + 2) << (pLSB - 1);
+      }
+      dp[block->blksampl_stride] = val;
+      vp[0]                      = prev_v_n | v_n;
+      prev_v_n                   = 0;
+      ++dp;
+      if (++x >= block->size.x) {
+        ++vp;
+        break;
+      }
+
+      val = 0;
+      bit = 2;
+      if (inf & (1 << (4 + bit))) {
+        // get 32 bits of magsgn data
+        uint32_t ms_val = MagSgn.fetch();
+        uint32_t m_n    = U_q - ((inf >> (12 + bit)) & 1);  // remove e_k
+        MagSgn.advance(m_n);                                // consume m_n
+
+        val = ms_val << 31;                      // get sign bit
+        v_n = ms_val & ((1 << m_n) - 1);         // keep only m_n bits
+        v_n |= ((inf >> (8 + bit)) & 1) << m_n;  // add EMB e_1 as MSB
+        v_n |= 1;                                // add center of bin
+        // v_n now has 2 * (\mu - 1) + 0.5 with correct sign bit
+        // add 2 to make it 2*\mu+0.5, shift it up to missing MSBs
+        val |= (v_n + 2) << (pLSB - 1);
+      }
+      dp[0] = val;
+
+      v_n = 0;
+      val = 0;
+      bit = 3;
+      if (inf & (1 << (4 + bit))) {
+        // get 32 bits of magsgn data
+        uint32_t ms_val = MagSgn.fetch();
+        uint32_t m_n    = U_q - ((inf >> (12 + bit)) & 1);  // remove e_k
+        MagSgn.advance(m_n);                                // consume m_n
+
+        val = ms_val << 31;                      // get sign bit
+        v_n = ms_val & ((1 << m_n) - 1);         // keep only m_n bits
+        v_n |= ((inf >> (8 + bit)) & 1) << m_n;  // add EMB e_1 as MSB
+        v_n |= 1;                                // add center of bin
+        // v_n now has 2 * (\mu - 1) + 0.5 with correct sign bit
+        // add 2 to make it 2*\mu+0.5, shift it up to missing MSBs
+        val |= (v_n + 2) << (pLSB - 1);
+      }
+      dp[block->blksampl_stride] = val;
+      prev_v_n                   = v_n;
+      ++dp;
+      ++x;
+    }
+    vp[0] = prev_v_n;
+
+    for (uint32_t y = 2; y < block->size.y; y += 2) {
+      uint16_t *sp = scratch + (y >> 1) * sstr;
+      uint32_t *vp = v_n_scratch;
+      int32_t *dp  = block->sample_buf + y * block->blksampl_stride;
+
+      prev_v_n = 0;
+      for (uint32_t x = 0; x < block->size.x; sp += 2, ++vp) {
+        uint32_t inf = sp[0];
+        uint32_t u_q = sp[1];
+
+        uint32_t gamma = inf & 0xF0;
+        gamma &= gamma - 0x10;  // is gamma_q 1?
+        uint32_t emax  = vp[0] | vp[1];
+        emax           = 31 - count_leading_zeros(emax | 2);  // emax - 1
+        uint32_t kappa = gamma ? emax : 1;
+
+        uint32_t U_q = u_q + kappa;
+        if (U_q > ((30 - pLSB) + 2)) {
+          printf("ERROR\n");
+        }
+
+        uint32_t v_n;
+        uint32_t val = 0;
+        uint32_t bit = 0;
+        if (inf & (1 << (4 + bit))) {
+          // get 32 bits of magsgn data
+          uint32_t ms_val = MagSgn.fetch();
+          uint32_t m_n    = U_q - ((inf >> (12 + bit)) & 1);  // remove e_k
+          MagSgn.advance(m_n);                                // consume m_n
+
+          val = ms_val << 31;                      // get sign bit
+          v_n = ms_val & ((1 << m_n) - 1);         // keep only m_n bits
+          v_n |= ((inf >> (8 + bit)) & 1) << m_n;  // add EMB e_1 as MSB
+          v_n |= 1;                                // add center of bin
+          // v_n now has 2 * (\mu - 1) + 0.5 with correct sign bit
+          // add 2 to make it 2*\mu+0.5, shift it up to missing MSBs
+          val |= (v_n + 2) << (pLSB - 1);
+        }
+        dp[0] = val;
+
+        v_n = 0;
+        val = 0;
+        bit = 1;
+        if (inf & (1 << (4 + bit))) {
+          // get 32 bits of magsgn data
+          uint32_t ms_val = MagSgn.fetch();
+          uint32_t m_n    = U_q - ((inf >> (12 + bit)) & 1);  // remove e_k
+          MagSgn.advance(m_n);                                // consume m_n
+
+          val = ms_val << 31;                      // get sign bit
+          v_n = ms_val & ((1 << m_n) - 1);         // keep only m_n bits
+          v_n |= ((inf >> (8 + bit)) & 1) << m_n;  // add EMB e_1 as MSB
+          v_n |= 1;                                // add center of bin
+          // v_n now has 2 * (\mu - 1) + 0.5 with correct sign bit
+          // add 2 to make it 2*\mu+0.5, shift it up to missing MSBs
+          val |= (v_n + 2) << (pLSB - 1);
+        }
+        dp[block->blksampl_stride] = val;
+        vp[0]                      = prev_v_n | v_n;
+        prev_v_n                   = 0;
+        ++dp;
+        if (++x >= block->size.x) {
+          ++vp;
+          break;
+        }
+
+        val = 0;
+        bit = 2;
+        if (inf & (1 << (4 + bit))) {
+          // get 32 bits of magsgn data
+          uint32_t ms_val = MagSgn.fetch();
+          uint32_t m_n    = U_q - ((inf >> (12 + bit)) & 1);  // remove e_k
+          MagSgn.advance(m_n);                                // consume m_n
+
+          val = ms_val << 31;                      // get sign bit
+          v_n = ms_val & ((1 << m_n) - 1);         // keep only m_n bits
+          v_n |= ((inf >> (8 + bit)) & 1) << m_n;  // add EMB e_1 as MSB
+          v_n |= 1;                                // add center of bin
+          // v_n now has 2 * (\mu - 1) + 0.5 with correct sign bit
+          // add 2 to make it 2*\mu+0.5, shift it up to missing MSBs
+          val |= (v_n + 2) << (pLSB - 1);
+        }
+        dp[0] = val;
+
+        v_n = 0;
+        val = 0;
+        bit = 3;
+        if (inf & (1 << (4 + bit))) {
+          // get 32 bits of magsgn data
+          uint32_t ms_val = MagSgn.fetch();
+          uint32_t m_n    = U_q - ((inf >> (12 + bit)) & 1);  // remove e_k
+          MagSgn.advance(m_n);                                // consume m_n
+
+          val = ms_val << 31;                      // get sign bit
+          v_n = ms_val & ((1 << m_n) - 1);         // keep only m_n bits
+          v_n |= ((inf >> (8 + bit)) & 1) << m_n;  // add EMB e_1 as MSB
+          v_n |= 1;                                // add center of bin
+          // v_n now has 2 * (\mu - 1) + 0.5 with correct sign bit
+          // add 2 to make it 2*\mu+0.5, shift it up to missing MSBs
+          val |= (v_n + 2) << (pLSB - 1);
+        }
+        dp[block->blksampl_stride] = val;
+        prev_v_n                   = v_n;
+        ++dp;
+        ++x;
+      }
+      vp[0] = prev_v_n;
+    }
+  }
+}
+
+void ht_cleanup_decode2(j2k_codeblock *block, const uint8_t &pLSB, const int32_t Lcup, const int32_t Pcup,
+                        const int32_t Scup) {
   fwd_buf<0xFF> MagSgn(block->get_compressed_data(), Pcup);
   MEL_dec MEL(block->get_compressed_data(), Lcup, Scup);
   rev_buf VLC_dec(block->get_compressed_data(), Lcup, Scup);
